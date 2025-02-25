a5662606212688a97444ee1ac8de013256e618d8
Shahriyar Mammadov <shahriyarmammadov16@gmail.com>
2025-02-25 15:19:05 +0400

    add events for private inspect

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ import "aos/dist/aos.css";
 function App() {
   const router = createBrowserRouter(ROUTES);
 
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault();
+    }
+});
+
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+});
+
+
   return <RouterProvider router={router}></RouterProvider>;
 }
 
